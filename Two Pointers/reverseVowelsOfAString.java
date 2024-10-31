48ae9f9e46793b32dc3e7f5517b259db32e7d868
Diya <shaikh.diya@northsouth.edu>
2024-10-31 16:19:04 -0400

    added new folder and problems solution (unsolved)

---
+++ Two Pointers/reverseVowelsOfAString.java
@@ -0,0 +1,32 @@
+
+import java.lang.*;
+
+public class reverseVowelsOfAString {
+    public static void main(String[] args) {
+        String str = "IceCreAm";
+        StringBuilder s = new StringBuilder(str);
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            System.out.println("at first right = " + right);
+            System.out.println("at first left = " + left);
+            if (str.charAt(left) != 'I' || str.charAt(left) != 'e' || str.charAt(left) != 'A') {
+                left++;
+            }
+            if (str.charAt(right) != 'I' || str.charAt(right) != 'e' || str.charAt(right) != 'A') {
+                right--;
+
+            }
+            System.out.println("right = " + right);
+            System.out.println("left = " + left);
+            char temp = s.charAt(left);
+            s.insert(left, s.charAt(right));
+            s.insert(left, temp);
+
+        }
+        str = s.toString();
+        System.out.println(str);
+
+    }
+}
