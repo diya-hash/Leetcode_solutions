88a2132eaeecd4fb98bc776cbfa04a5ab6baf45f
Diya <shaikh.diya@northsouth.edu>
2024-10-30 23:08:17 -0400

    accepted solution

--- Strings/ReverseString.java
+++ Strings/ReverseString.java
@@ -1,22 +1,15 @@
 public class ReverseString {
-    public static void swap(char[] s, int left, int right) {
-        char temp = s[left];
-        s[left] = s[right];
-        s[right] = temp;
-    }
 
     public static void main(String[] args) {
         char[] s = { 'h', 'e', 'l', 'p' };
         int left = 0;
         int right = s.length - 1;
         while (left < right) {
-            System.out.println("left: " + left);
-            System.out.println("right" + right);
-            swap(s, left, right);
-            System.out.println(s);
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
             left++;
             right--;
-
         }
         System.out.println(s);
     }
