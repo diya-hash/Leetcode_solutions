024bcfefa92aa3af499378476f6a111bc3cd6665
Diya Salam <sdiya.salam.github@gmail.com>
2024-11-02 15:18:46 -0400

    accepted solution

---
+++ Strings/balancedStrings.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+
+public class balancedStrings {
+    public static void main(String[] args) {
+        String s = "RLRRRLLRLL";
+        int r = 0;
+        int l = 0;
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'R')
+                r++;
+            else if (s.charAt(i) == 'L')
+                l++;
+
+            if (r == l) {
+                count++;
+                r = 0;
+                l = 0;
+            }
+
+        }
+        System.out.println(count);
+    }
+
+}
