e31b6d5a78dc8716ec8b819e1663eda701352c5c
Diya Salam <sdiya.salam.github@gmail.com>
2024-11-05 13:34:06 -0500

    accepted solution

--- Arrays/largestLocalValues.java
+++ Arrays/largestLocalValues.java
@@ -1,7 +1,11 @@
+
 //problem link: https://leetcode.com/problems/largest-local-values-in-a-matrix/description/
+import java.util.*;
+
 public class largestLocalValues {
     public static void main(String[] args) {
         int[][] grid = { { 9, 9, 8, 1 }, { 5, 6, 2, 6 }, { 8, 2, 6, 4 }, { 6, 2, 2, 2 } };
+        int[][] result = new int[grid.length - 2][grid.length - 2];
         int count = 0;
 
         for (int row = 0; row < grid.length; row++) {
@@ -21,14 +25,20 @@ public class largestLocalValues {
                     }
                 }
                 if (max > -1) {
-                    System.out.println("row = " + row + ", col = " + col);
-
-                    System.out.print(max + "\t");
+                    // System.out.println("row = " + row + ", col = " + col);
+                    result[row][col] = max;
+                    // System.out.print(max + "\t");
                 }
 
                 System.out.println("");
             }
 
+        }
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + "\t");
+            }
+            System.out.println("");
         }
     }
 
