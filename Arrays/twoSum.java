92bcfa35aac2ddcae4e069928776874c8e045729
Diya Salam <sdiya.salam.github@gmail.com>
2024-11-10 13:54:08 -0500

    accepted solution

---
+++ Arrays/twoSum.java
@@ -0,0 +1,25 @@
+
+//problem link: https://leetcode.com/problems/two-sum/submissions/1448936680/
+import java.util.*;
+
+public class twoSum {
+    public static void main(String[] args) {
+        int[] nums = { 0, 4, 3, 0 };
+        int target = 0;
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // using O(logn):
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[0] = i;
+                result[1] = map.get(target - nums[i]);
+            }
+            map.put(nums[i], i);
+        }
+
+        System.out.println(map);
+        for (int i = 0; i < result.length; i++)
+            System.out.println(result[i]);
+    }
+}
