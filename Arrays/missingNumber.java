c160335644fcfd333091216501667a40d796830f
Diya Salam <sdiya.salam.github@gmail.com>
2024-11-11 14:25:37 -0500

    accepted solution

---
+++ Arrays/missingNumber.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class missingNumber {
+    public static void main(String[] args) {
+        int[] nums = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+        int[] occurs = new int[nums.length + 1];
+        int result = 0;
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            occurs[nums[i]]++;
+        }
+        for (int i = 0; i <= nums.length; i++) {
+            if (occurs[i] == 0)
+                result = i;
+        }
+        System.out.println(result);
+    }
+
+}
