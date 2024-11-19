97c78beeedd771f5d76d9fc8a5fdb1f2068e7212
Diya Salam <sdiya.salam.github@gmail.com>
2024-11-18 23:04:22 -0500

    accepted solution: cleaned the code

--- Blind75/Matrix/prodOfArrayExceptItself.java
+++ Blind75/Matrix/prodOfArrayExceptItself.java
@@ -5,29 +5,24 @@ public class prodOfArrayExceptItself {
         int[] nums = { 1, 2, 3, 4 };
         int[] suff_arr = new int[nums.length];
         int[] result = new int[nums.length];
-        int prod = 1;
         int prefix = 1;
         int suffix = 1;
 
         suffix = nums[nums.length - 1];
         nums[nums.length - 1] = 1;
         suff_arr[nums.length - 1] = 1;
-        System.out.println(suff_arr[nums.length - 1]);
         for (int i = nums.length - 2; i >= 0; i--) {
             suffix *= nums[i + 1];
             suff_arr[i] = suffix;
-            System.out.println(suff_arr[i]);
         }
         for (int i = 0; i < nums.length; i++) {
             if (i == 0)
                 prefix = 1;
             else
                 prefix *= nums[i - 1];
-            // removed suffix calculation
             // System.out.println("i: " + i + " prefix: " + prefix + " suffix: " +
             // suff_arr[i]);
-            prod = prefix * suff_arr[i];
-            result[i] = prod;
+            result[i] = prefix * suff_arr[i];
 
         }
         for (int i = 0; i < suff_arr.length; i++) {
