75ad932cd00ba46fbbb78784a76a0dd76ecb2d40
Diya Salam <sdiya.salam.github@gmail.com>
2024-11-18 22:50:31 -0500

    accepted solution but need to optimize memory/space complexity

--- Blind75/Matrix/prodOfArrayExceptItself.java
+++ Blind75/Matrix/prodOfArrayExceptItself.java
@@ -2,30 +2,35 @@ package Leetcode_solutions.Blind75.Matrix;
 
 public class prodOfArrayExceptItself {
     public static void main(String[] args) {
-        int[] nums = { -1, 1, 0, -3, 3 };
+        int[] nums = { 1, 2, 3, 4 };
+        int[] suff_arr = new int[nums.length];
         int[] result = new int[nums.length];
         int prod = 1;
         int prefix = 1;
         int suffix = 1;
 
+        suffix = nums[nums.length - 1];
+        nums[nums.length - 1] = 1;
+        suff_arr[nums.length - 1] = 1;
+        System.out.println(suff_arr[nums.length - 1]);
+        for (int i = nums.length - 2; i >= 0; i--) {
+            suffix *= nums[i + 1];
+            suff_arr[i] = suffix;
+            System.out.println(suff_arr[i]);
+        }
         for (int i = 0; i < nums.length; i++) {
             if (i == 0)
                 prefix = 1;
             else
                 prefix *= nums[i - 1];
-            suffix = 1;
-            for (int j = i + 1; j < nums.length; j++) {
-                if (i == nums.length - 1)
-                    suffix = 1;
-                else
-                    suffix *= nums[j];
-            }
-            System.out.println("i: " + i + " prefix: " + prefix + " suffix: " + suffix);
-            prod = prefix * suffix;
+            // removed suffix calculation
+            // System.out.println("i: " + i + " prefix: " + prefix + " suffix: " +
+            // suff_arr[i]);
+            prod = prefix * suff_arr[i];
             result[i] = prod;
 
         }
-        for (int i = 0; i < result.length; i++) {
+        for (int i = 0; i < suff_arr.length; i++) {
             System.out.println(result[i]);
         }
     }
