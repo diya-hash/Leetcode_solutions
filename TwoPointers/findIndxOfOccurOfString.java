5f4d87cc8be71ad505098a6b5b915379df4a77bd
Diya Salam <sdiya.salam.github@gmail.com>
2024-11-22 15:15:05 -0500

    accepted solution

---
+++ TwoPointers/findIndxOfOccurOfString.java
@@ -0,0 +1,27 @@
+package Leetcode_solutions.TwoPointers;
+
+public class findIndxOfOccurOfString {
+    public static void main(String[] args) {
+        String haystack = "baabbbbababbbabab";
+        String needle = "abbab";
+        int result = -1;
+
+        if (needle.length() > haystack.length())
+            result = -1;
+        else if (haystack.equals(needle))
+            result = 0;
+        else {
+            for (int i = 0; i < haystack.length(); i++) {
+                int right = i + needle.length();
+                if (right <= haystack.length()) {
+                    if (haystack.substring(i, right).equals(needle)) {
+                        result = i;
+                        System.out.println(needle);
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
