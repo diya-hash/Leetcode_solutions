c122403e8b5d0225c71af466801ede8804790071
Diya Salam <sdiya.salam.github@gmail.com>
2024-11-25 22:08:34 -0500

    accepted solution

---
+++ TwoPointers/minAvgSmallestLargestElement.java
@@ -0,0 +1,26 @@
+package Leetcode_solutions.TwoPointers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class minAvgSmallestLargestElement {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 7, 8, 9 };
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+        double min = 100000;
+
+        while (left < right) {
+            double avg = (nums[left] + nums[right]) / 2.0;
+            if (avg < min) {
+                min = avg;
+            }
+            left++;
+            right--;
+        }
+        System.out.println(min);
+    }
+}
+// problem link:
+// https://leetcode.com/problems/minimum-average-of-smallest-and-largest-elements/
