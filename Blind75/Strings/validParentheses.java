d14b5e9d25f9f74646802a8cbed53a5e160c2917
Diya Salam <sdiya.salam.github@gmail.com>
2024-11-26 21:55:55 -0500

    accepted solution

--- Blind75/Strings/validParentheses.java
+++ Blind75/Strings/validParentheses.java
@@ -5,73 +5,29 @@ import java.util.Stack;
 public class validParentheses {
 
     public static void main(String[] args) {
-        String s = "([])";
+        String s = "(){}}{";
         Stack<Character> stack = new Stack<>();
-        int sq = 0;
-        int c = 0;
-        int b = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            stack.push(s.charAt(i));
-            if (s.charAt(i) == '(') {
-                b++;
+        stack.push(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            if (!stack.isEmpty() && s.charAt(i) == ')' && stack.peek() == '(' ||
+                    !stack.isEmpty() && s.charAt(i) == ']' && stack.peek() == '[' ||
+                    !stack.isEmpty() && s.charAt(i) == '}' && stack.peek() == '{') {
+                System.out.println(stack.pop());
+            } else {
                 stack.push(s.charAt(i));
-
-            } else if (s.charAt(i) == ')') {
-                b--;
-                stack.pop();
-
             }
-            // } else if (s.charAt(i) == '[') {
-            // sq++;
-            // stack.push(s.charAt(i));
-            // } else if (s.charAt(i) == '{') {
-            // c++;
-            // stack.push(s.charAt(i));
-            // }
-
         }
 
-        while (!stack.isEmpty()) {
-            System.out.println(stack.pop());
+        if (stack.isEmpty()) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
         }
 
     }
 }
-// int curls = 0;
-// int bracks = 0;
-// int squares = 0;
-// boolean cflag = false;
-// boolean sflag = false;
-// boolean bflag = false;
-
-// for (int i = 0; i < s.length(); i++) {
-// if (s.charAt(i) == '{') {
-// cflag = true;
-// curls++;
-// } else if (s.charAt(i) == '}') {
-// curls--;
-// if (curls == 0)
-// cflag = false;
-// } else if (s.charAt(i) == '[') {
-// squares++;
-// sflag = true;
-// } else if (s.charAt(i) == ']') {
-// squares--;
-// if (squares == 0)
-// sflag = false;
-// } else if (s.charAt(i) == '(') {
-// bracks++;
-// bflag = true;
-// } else if (s.charAt(i) == ')') {
-// bracks--;
-// if (bracks == 0)
-// bflag = false;
-// }
-
-// }
-// if (cflag == false && bflag == false && sflag == false) {
-// System.out.println("Valid");
+// if (stack.peek() == s.charAt(i)) {
+// System.out.println("exists: " + stack.pop());
 // } else {
-// System.out.println("Invalid");
-// }
+// stack.push(s.charAt(i));
+// // }
