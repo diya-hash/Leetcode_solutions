0c4cc5d34d119c81149dea759ccd63cfda22f46b
Diya Salam <sdiya.salam.github@gmail.com>
2024-11-28 10:56:24 -0500

    accepted solution - beats 100% - solution post given

--- TwoPointers/squaresOfASortedArray.java
+++ TwoPointers/squaresOfASortedArray.java
@@ -3,38 +3,97 @@ package Leetcode_solutions.TwoPointers;
 public class squaresOfASortedArray {
 
     public static void main(String[] args) {
-        int[] nums = { -5, -3, -2, -1 };
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = nums[i] * nums[i];
-            System.out.println(nums[i]);
-        }
-
-        int left = 0;
-        int right = nums.length - 1;
-        int count = 0;
-        while (left != right) {
-            System.out.println("entered the while loop");
-            if (nums[left] > nums[right]) {
+        int[] nums = { -1 };
+        int[] result = new int[nums.length];
+        int j = 0;
+        if (nums[0] >= 0) {
+            for (int i = 0; i < nums.length; i++) {
+                result[i] = nums[i] * nums[i];
+            }
+        } else if (nums[nums.length - 1] <= 0) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left < right) {
                 int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                if (nums[right] > nums[right - 1])
-                    right--;
-                if (nums[left] < nums[left + 1])
-                    left++;
-
-            } else if (nums[left] < nums[right]) {
+                nums[left] = nums[right] * nums[right];
+                nums[right] = temp * temp;
+                left++;
                 right--;
             }
-            count++;
-            for (int j = 0; j < nums.length; j++)
-                System.out.print(nums[j] + "\t");
-            System.out.println("");
-            System.out.println("i: " + count + " left: " + left + " right: " + right);
+            if (left == right) {
+                nums[right] = nums[right] * nums[right];
+            }
+            for (int i = 0; i < nums.length; i++) {
+                result[j] = nums[i];
+                j++;
+            }
+
+        } else {
+            // System.out.println("entered last else condiion");
+            int left = 0;
+            int right = 0;
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] < 0 && nums[i + 1] >= 0) {
+                    left = i;
+                    right = i + 1;
+                    break;
+                }
+            }
+            // System.out.println("left = " + left + " right = " + right);
+
+            while (left >= 0 || right <= nums.length - 1) {
+                // System.out.println("entered while loop");
+                nums[left] = Math.abs(nums[left]);
+                nums[right] = Math.abs(nums[right]);
+                if (nums[left] < nums[right]) {
+                    result[j] = nums[left] * nums[left];
+                    j++;
+                    left--;
+                    if (left < 0)
+                        break;
+                } else if (nums[left] > nums[right]) {
+                    result[j] = nums[right] * nums[right];
+                    j++;
+                    right++;
+                    if (right == nums.length)
+                        break;
+                } else {
+                    result[j] = nums[left] * nums[left];
+                    j++;
+                    result[j] = nums[right] * nums[right];
+                    j++;
+                    right++;
+                    left--;
+                    if (right == nums.length || left < 0)
+                        break;
+                }
+
+                // System.out.println("left = " + left + " right = " + right);
+                // for (int i = 0; i < result.length; i++) {
+                // System.out.print(result[i] + "\t");
+                // }
+            }
+            if (left == -1 && right < nums.length) {
+                // System.out.println("reached left=-1 condition");
+                for (int i = right; i < nums.length; i++) {
+                    result[j] = nums[i] * nums[i];
+                    j++;
+                }
+            } else {
+                // System.out.println("reached right>=nums.length condition");
+                for (int i = left; i >= 0; i--) {
+                    result[j] = nums[i] * nums[i];
+                    j++;
+                }
+            }
+
+            // System.out.println("left = " + left + " right = " + right);
         }
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + "\t");
         }
+
     }
 }
 // problem link:unsolved
