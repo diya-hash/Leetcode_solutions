a74828d5b553533986ab30f44bb0622e4c19b43d
Diya Salam <sdiya.salam.github@gmail.com>
2024-12-02 22:23:14 -0500

    accepted - Beats 100% - solution post given

---
+++ TwoPointers/reverseOnlyLetters.java
@@ -0,0 +1,40 @@
+public class reverseOnlyLetters {
+    public static void main(String[] args) {
+        String s = "7_28]";
+        int l = 0;
+        int r = s.length() - 1;
+        char[] ch = s.toCharArray();
+        boolean isFlag = false;
+        while (l < r) {
+
+            while (l < r && (ch[l] >= '!' && ch[l] <= '@')
+                    || (ch[l] >= '[' && ch[l] <= '`')) {
+                l++;
+                if (l == s.length()) {
+                    isFlag = true;
+                    break;
+                }
+
+            }
+            while (l < r && (ch[r] >= '!' && ch[r] <= '@')
+                    || (ch[r] >= '[' && ch[r] <= '`')) {
+                r--;
+                if (r < 0) {
+                    isFlag = true;
+                    break;
+                }
+            }
+            if (isFlag == true)
+                break;
+            char temp = ch[l];
+            ch[l] = ch[r];
+            ch[r] = temp;
+            l++;
+            r--;
+        }
+        s = String.valueOf(ch);
+        System.out.println(s);
+    }
+}
+// solution post:
+// https://leetcode.com/problems/reverse-only-letters/solutions/6106789/java-solution-two-pointer-beats-100
