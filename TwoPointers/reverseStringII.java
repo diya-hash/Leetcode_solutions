10ddb9ccf4333b82d9969ded7803c8facf4e0520
Diya Salam <sdiya.salam.github@gmail.com>
2024-12-02 22:44:38 -0500

    inconplete

---
+++ TwoPointers/reverseStringII.java
@@ -0,0 +1,8 @@
+
+public class reverseStringII {
+    public static void main(String[] args) {
+        // take substring of 2k and reverse the first k
+        // if there are less then 2k but equal to k then reverse upto length k
+        // if there less thaan k then reverse alll of them
+    }
+}
