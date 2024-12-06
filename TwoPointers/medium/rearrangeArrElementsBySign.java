4875e95c930acdaa6ac428c0ddbb79a474ba223f
Diya Salam <sdiya.salam.github@gmail.com>
2024-12-06 16:39:31 -0500

    accepted: optimized

--- TwoPointers/medium/rearrangeArrElementsBySign.java
+++ TwoPointers/medium/rearrangeArrElementsBySign.java
@@ -4,27 +4,19 @@ public class rearrangeArrElementsBySign {
     public static void main(String[] args) {
         int[] nums = { 3, 1, -2, -5, 2, -4 };
         int j = 0;
+        int k = 1;
         int[] arr = new int[nums.length];
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > 0) {
                 arr[j] = nums[i];
                 j += 2;
-                if (j == arr.length - 1) {
-                    break;
-                }
+            } else if (nums[i] < 0) {
+                arr[k] = nums[i];
+                k += 2;
             }
         }
-        j = 1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < 0) {
-                arr[j] = nums[i];
-                j += 2;
-                if (j == arr.length) {
-                    break;
-                }
-            }
-        }
+
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
