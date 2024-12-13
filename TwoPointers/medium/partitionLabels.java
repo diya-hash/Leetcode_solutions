40306fc462bf26c8866ea065952ba92a76eacebd
Diya Salam <sdiya.salam.github@gmail.com>
2024-12-12 22:07:38 -0500

    accepted

--- TwoPointers/medium/partitionLabels.java
+++ TwoPointers/medium/partitionLabels.java
@@ -5,32 +5,24 @@ import java.util.ArrayList;
 public class partitionLabels {
     public static void main(String[] args) {
         String s = "caedbdedda";
-        int[] lastIndx = new int[26];
+        char[] str = s.toCharArray();
+        int[] lastIndx = new int[256];
         ArrayList<Integer> result = new ArrayList<>();
         int start = 0;
         int end = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            int num = (int) s.charAt(i) - 'a';
-            lastIndx[num] = i;
+            lastIndx[str[i]] = i;
         }
-        // for (int i = 0; i < 26; i++) {
-        // System.out.println(i + "\t" + lastIndx[i]);
-        // }
         for (int i = 0; i < s.length(); i++) {
-            int num = (int) s.charAt(i) - 'a';
-            if (lastIndx[num] > end) {
-                end = lastIndx[num];
-                System.out.println(end);
+            if (lastIndx[str[i]] > end) {
+                end = lastIndx[str[i]];
             }
             if (end == i) {
                 result.add((end - start) + 1);
-                System.out.println(result);
                 start = end + 1;
-                end = 0;
             }
-
         }
-        // System.out.println(result);
+        System.out.println(result);
     }
 }
