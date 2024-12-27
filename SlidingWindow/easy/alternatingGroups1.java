7ee6744b26a669b393166628d72fa943ce03362f
Diya Salam <sdiya.salam.github@gmail.com>
2024-12-27 10:47:14 -0500

    accepted: beats 100%

--- SlidingWindow/easy/alternatingGroups1.java
+++ SlidingWindow/easy/alternatingGroups1.java
@@ -5,20 +5,16 @@ import java.util.Arrays;
 public class alternatingGroups1 {
     public static void main(String[] args) {
         int[] colors = { 0, 1, 0, 0, 1 };
-        int left = colors.length - 1;
-        int right = 1;
+        int n = colors.length;
         int alternatingGroups = 0;
-        for (int i = 0; i < colors.length; i++) {
-            if (colors[left] != colors[i] && colors[i] != colors[right]) {
+        for (int i = 0; i < n - 2; i++) {
+            if (colors[i] == colors[i + 2] && colors[i] != colors[i + 1])
                 alternatingGroups++;
-            }
-            right++;
-            left++;
-            if (left >= colors.length)
-                left = left % colors.length;
-            else if (right >= colors.length)
-                right = right % colors.length;
         }
+        if (colors[0] == colors[n - 2] && colors[0] != colors[n - 1])
+            alternatingGroups++;
+        if (colors[1] == colors[n - 1] && colors[1] != colors[0])
+            alternatingGroups++;
         System.out.println(alternatingGroups);
     }
 }
