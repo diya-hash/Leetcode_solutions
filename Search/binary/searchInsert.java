46e84575a77a696e6516e9d5f4cba45fd0390f51
Diya Salam <sdiya.salam.github@gmail.com>
2025-01-19 22:32:10 -0500

    accepted

--- Search/binary/searchInsert.java
+++ Search/binary/searchInsert.java
@@ -3,14 +3,12 @@ package Search.binary;
 public class searchInsert {
     public static void main(String[] args) {
         int[] nums = { 1, 3, 5, 6 };
-        int target = 7;
+        int target = 0;
 
         int left = 0;
         int right = nums.length - 1;
-        int index = 0;
         while (left <= right) {
             int mid = (left + right) / 2;
-            index = mid;
             if (nums[mid] == target) {
                 System.out.println(mid);
                 break;
@@ -19,13 +17,7 @@ public class searchInsert {
             } else {
                 right = mid - 1;
             }
-            System.out.println(index);
         }
-        if (nums[index] < target)
-            System.out.println(index + 1);
-        else if (nums[index] > target)
-            System.out.println(index);
-        else
-            System.out.println(index);
+        System.out.println(left);
     }
 }
