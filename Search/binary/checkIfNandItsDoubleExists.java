cb70515365bf721ea285422dad6548e4445e11aa
Diya Salam <sdiya.salam.github@gmail.com>
2025-01-22 00:45:13 -0500

    optimized: Beats 90%

--- Search/binary/checkIfNandItsDoubleExists.java
+++ Search/binary/checkIfNandItsDoubleExists.java
@@ -3,33 +3,33 @@ package Search.binary;
 import java.util.Arrays;
 
 public class checkIfNandItsDoubleExists {
-    static boolean binarySearch(int[] arr, int target, int position) {
-        int left = 0;
-        int right = arr.length - 1;
-        boolean isTrue = false;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] == target && mid != position) {
-                isTrue = true;
-                break;
-            } else if (arr[mid] > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return isTrue;
-    }
 
     public static void main(String[] args) {
-
-        int[] arr = { -16, -13, 8 };
+        int[] arr = { 3, 1, 7, 11 };
+        int n = arr.length;
         Arrays.sort(arr);
         boolean isTrue = false;
-        for (int i = 0; i < arr.length; i++) {
-            if (binarySearch(arr, arr[i] * 2, i)) {
-                isTrue = true;
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] % 2 != 0) {
+                continue;
+            } else {
+                int target = arr[i] / 2;
+                int left = 0;
+                int right = n - 1;
+                while (left <= right) {
+                    int mid = left + (right - left) / 2;
+                    if (arr[mid] == target && mid != i) {
+                        isTrue = true;
+                        break;
+                    } else if (arr[mid] > target) {
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1;
+                    }
+                }
             }
+            if (isTrue == true)
+                break;
         }
         System.out.println(isTrue);
     }
