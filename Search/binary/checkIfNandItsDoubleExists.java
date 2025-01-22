9e86aa2aef028e1dd3c3d5d973e7fa2ddd9b02dc
Diya Salam <sdiya.salam.github@gmail.com>
2025-01-22 00:30:03 -0500

    accepted: binarysearch

--- Search/binary/checkIfNandItsDoubleExists.java
+++ Search/binary/checkIfNandItsDoubleExists.java
@@ -1,20 +1,37 @@
 package Search.binary;
 
 import java.util.Arrays;
-import java.util.HashSet;
 
 public class checkIfNandItsDoubleExists {
-    public static void main(String[] args) {
-        int[] arr = { -20, 8, -6, -14, 0, -19, 14, 4 };
-        HashSet<Integer> set = new HashSet<>();
+    static boolean binarySearch(int[] arr, int target, int position) {
+        int left = 0;
+        int right = arr.length - 1;
         boolean isTrue = false;
-        for (int i : arr) {
-            if (set.contains(i * 2) || (set.contains(i / 2) && i % 2 == 0)) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target && mid != position) {
                 isTrue = true;
                 break;
+            } else if (arr[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
-            set.add(i);
+        }
+        return isTrue;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { -16, -13, 8 };
+        Arrays.sort(arr);
+        boolean isTrue = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (binarySearch(arr, arr[i] * 2, i)) {
+                isTrue = true;
+            }
         }
         System.out.println(isTrue);
     }
 }
+// -16, -13, 8
