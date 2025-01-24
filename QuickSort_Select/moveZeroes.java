c1e6af138448f73b1c345087dbeab16bc30ea44e
Diya Salam <sdiya.salam.github@gmail.com>
2025-01-23 22:04:18 -0500

    optimized

--- QuickSort_Select/moveZeroes.java
+++ QuickSort_Select/moveZeroes.java
@@ -2,18 +2,18 @@ package QuickSort_Select;
 
 public class moveZeroes {
     public static void main(String[] args) {
-        int[] nums = { 0 };
-        int j = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
+        int[] nums = { 2, 1 };
+        int i = -1;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+                i = i + 1;
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
-                j++;
             }
         }
-        for (int i : nums) {
-            System.out.print(i + " ");
+        for (int n : nums) {
+            System.out.print(n + " ");
         }
     }
 }
