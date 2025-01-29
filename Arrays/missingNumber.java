e6c8f4867479d3ceb20dc82ceb0701c1a9ecc07e
Diya Salam <sdiya.salam.github@gmail.com>
2025-01-29 00:33:36 -0500

    acceepted: Hashtable

--- Arrays/missingNumber.java
+++ Arrays/missingNumber.java
@@ -1,23 +1,21 @@
-
-//problem link: https://leetcode.com/problems/missing-number/submissions/1449894453/
-package Leetcode_solutions.Arrays;
-
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public class missingNumber {
     public static void main(String[] args) {
         int[] nums = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
-        int[] occurs = new int[nums.length + 1];
-        int result = 0;
-        int j = 0;
+        HashSet<Integer> set = new HashSet<>();
+        int missingNum = 0;
+        for (int i : nums) {
+            set.add(i);
+        }
         for (int i = 0; i < nums.length; i++) {
-            occurs[nums[i]]++;
+            if (!set.contains(i + 1)) {
+                missingNum = i + 1;
+            }
         }
-        for (int i = 0; i <= nums.length; i++) {
-            if (occurs[i] == 0)
-                result = i;
-        }
-        System.out.println(result);
+        System.out.println(missingNum);
     }
 
 }
