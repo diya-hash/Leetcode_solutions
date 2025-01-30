e2d3308a38791c0b7c24164eec31ace7a8c9cdc4
Diya Salam <sdiya.salam.github@gmail.com>
2025-01-30 09:44:38 -0500

    accepted

---
+++ Arrays/medium/groupAnagrams.java
@@ -0,0 +1,26 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class groupAnagrams {
+    public static void main(String[] args) {
+        String[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+        Map<String, List<String>> map = new HashMap<>();
+        List<List<String>> list = new ArrayList<>();
+        for (String word : strs) {
+            char[] ch = word.toCharArray();
+            Arrays.sort(ch);
+            String sortedWord = new String(ch);
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+            map.get(sortedWord).add(word);
+        }
+
+        System.out.println(map.values());
+    }
+}
