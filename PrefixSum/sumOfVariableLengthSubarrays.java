ca69323623bc410d93761c2901f4a54ed2984b83
Diya Salam <sdiya.salam.github@gmail.com>
2025-04-02 10:35:30 -0400

    add

--- PrefixSum/sumOfVariableLengthSubarrays.java
+++ PrefixSum/sumOfVariableLengthSubarrays.java
@@ -1,11 +1,19 @@
 public class sumOfVariableLengthSubarrays {
     public static void main(String[] args) {
-        int[] nums = { 3, 1, 1, 2 };
+        int[] nums = { 2, 3, 1 };
+        int[] prefixsum = new int[nums.length];
+        prefixsum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            prefixsum[i] = prefixsum[i - 1] + nums[i];
+            System.out.println(prefixsum[i]);
+        }
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
             int start = Math.max(0, i - nums[i]);
-            for (int j = start; j <= i; j++) {
-                sum += nums[j];
+            if (start == 0) {
+                sum += prefixsum[i];
+            } else {
+                sum += prefixsum[i] - prefixsum[start - 1];
             }
         }
         System.out.println(sum);
